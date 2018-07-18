7533f90b08fdcf91ce1631c936f98299a3a981c2
iwakiriK <balssearch0113@gmail.com>
2018-07-18 11:37:26 +0900

    Add 8_A - 8_B

---
+++ ALDS1/8_B.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i=0;i<(n);++i)
+using namespace std;
+
+struct Node {
+    int key;
+    Node *p, *left, *right;
+};
+Node *root, *NIL;
+
+Node* find(int k) {
+    Node *u = root;
+    while (u != NIL && u->key != k) {
+        if (k < u->key) u = u->left;
+        else u = u->right;
+    }
+    return u;
+}
+
+void insert(int k) {
+    Node *y = NIL;
+    Node *x = root;
+    while (x != NIL) {
+        y = x;
+        if (k < x->key) {
+            x = x->left;
+        } else {
+            x = x->right;
+        }
+    }
+    Node *z = new Node;
+    z->key = k;
+    z->p = y;
+    z->left = NIL;
+    z->right = NIL;
+    if (y == NIL) {
+        root = z;
+    } else {
+        if (z->key < y->key) {
+            y->left = z;
+        } else {
+            y->right = z;
+        }
+    }
+}
+
+void inorder(Node *u) {
+    if (u == NIL) return;
+    inorder(u->left);
+    cout << " " << u->key;
+    inorder(u->right);
+}
+void preorder(Node *u) {
+    if (u == NIL) return;
+    cout << " " << u->key;
+    preorder(u->left);
+    preorder(u->right);
+}
+
+int main() {
+    int n, k;
+    string com;
+    cin >> n;
+    rep (i, n) {
+        cin >> com;
+        if (com[0] == 'i') {
+            cin >> k;
+            insert(k);
+        } else if (com[0] == 'p'){
+            inorder(root);
+            cout << endl;
+            preorder(root);
+            cout << endl;
+        } else {
+            cin >> k;
+            Node *t = find(k);
+            if (t != NIL) cout << "yes" << endl;
+            else cout << "no" << endl;
+        }
+    }
+    
+    return 0;
+}
